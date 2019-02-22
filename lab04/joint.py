a21dea6ccdf2677d3038de365acc964eabe68410
bff3 <bff3@students.calvin.edu>
2019-02-22 10:23:11 -0500

    asdf

--- lab04/joint.py
+++ lab04/joint.py
@@ -20,3 +20,14 @@ P[F, F, T] = 0.144; P[F, F, F] = 0.576
 # Compute P(Cavity|Toothache=T)  (see the text, page 493).
 PC = enumerate_joint_ask('Cavity', {'Toothache': T}, P)
 print(PC.show_approx())
+#  p(Cavity|Catch) = .18/.34 = 0.529
+PCatch = enumerate_joint_ask('Cavity', {'Catch': T}, P)
+print(PCatch.show_approx())
+
+#T = heads, F = tails
+M = JointProbDist(['Coin1', 'Coin2'])
+M[T, T] = 0.25; M[T, F] = 0.25
+M[F, T] = 0.25; M[F, F] = 0.25
+
+MC = enumerate_joint_ask('Coin1', {'Coin2': T}, M)
+print(MC.show_approx())
